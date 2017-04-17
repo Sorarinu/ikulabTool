7d9ec20928bd70a5757a4235c1c6c65adce90fa4
高畑達也 <Sorarinu@Sorarinu-MacBook.local>
2017-04-17 15:44:22 +0900

    作業保存

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Log;
+use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function add(Requests\Request $request)
+    public function add(Request $request)
     {
         $sid = $request->input('studentId');
         $time = $request->input('time');
