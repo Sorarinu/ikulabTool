2743de78aade6bd0a9c913e2c1c2002ea920135b
高畑達也 <Sorarinu@Sorarinu-MacBook.local>
2017-04-17 15:39:00 +0900

    作業保存

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Log;
-use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function add(Request $request)
+    public function add(Requests\Request $request)
     {
         $sid = $request->input('studentId');
         $time = $request->input('time');
 
+
         Log::debug('sid: ' . $sid);
         Log::debug('time: ' . $time);
+
+        return response()->json(['status' => '200']);
     }
 }
